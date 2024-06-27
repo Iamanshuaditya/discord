a731cdbf9388bea05d37b4dbe47b9106a02a9805
Anshu Aditya <anshuaditya825301@gmail.com>
2024-06-27 14:40:45 +0530

    auth done

--- src/app/(auth)/(routes)/register/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Registerpage() {
-  return <div>Register</div>;
-}
-
-export default Registerpage;
--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,11 +1,9 @@
-import React from 'react'
+import React from "react";
 
-function AuthLayout({children}: {children: React.ReactNode}) {
+function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div>
-      {children}
-    </div>
-  )
+    <div className="h-full flex items-center justify-center">{children}</div>
+  );
 }
 
-export default AuthLayout
+export default AuthLayout;
