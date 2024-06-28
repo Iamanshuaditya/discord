46032681044cb1c1af2cbaba2454d217495ecdf3
Anshu Aditya <anshuaditya825301@gmail.com>
2024-06-28 06:28:30 +0530

    uploadthing error resolved

--- src/components/file-upload.tsx
+++ src/components/file-upload.tsx
@@ -2,6 +2,9 @@
 import { UploadDropzone } from "@/lib/uploadthing";
 import React from "react";
 import "@uploadthing/react/styles.css";
+import { X } from "lucide-react";
+import Image from "next/image";
+
 interface FileUploadProps {
   onChange: (url?: string) => void;
   value: string;
@@ -9,6 +12,14 @@ interface FileUploadProps {
 }
 
 function FileUpload({ onChange, value, endpoint }: FileUploadProps) {
+  const fileType = value?.split(".").pop();
+  if (value && fileType !== "pdf") {
+    return (
+      <div className="relative h-20 w-20">
+        <Image fill alt="Upload" src={value} className="rounded-full" />
+      </div>
+    );
+  }
   return (
     <UploadDropzone
       endpoint={endpoint}
