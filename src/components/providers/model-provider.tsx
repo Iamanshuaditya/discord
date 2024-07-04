7e8b5871be875721cb68c071eda4694ebe710f9d
Anshu Aditya <anshuaditya825301@gmail.com>
2024-07-04 23:12:23 +0530

    feat :edit channel

--- src/components/providers/model-provider.tsx
+++ src/components/providers/model-provider.tsx
@@ -8,6 +8,7 @@ import CreateChannelModal from "../models/create-channel-modal";
 import LeaveServerModal from "../models/leave-server-modal";
 import DeleteServerModal from "../models/delete-server-modal";
 import DeleteChannelModal from "../models/delete-channel-modal";
+import EditChannelModal from "../models/edit-channel-modal";
 
 export const ModelProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -29,6 +30,7 @@ export const ModelProvider = () => {
       <LeaveServerModal />
       <DeleteServerModal />
       <DeleteChannelModal />
+      <EditChannelModal />
     </>
   );
 };
